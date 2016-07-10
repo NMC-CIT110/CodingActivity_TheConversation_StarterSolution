7f1bacaa4bc45ae985d263a1d631485116ee5ab0
John Velis <john@johnvelis.com>
2016-07-10 09:32:37 -0400

    Initial Commit

---
+++ CodingActivity_TheConversation_StarterSolution/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CodingActivity_TheConversation_StarterSolution
+{
+    class Program
+    {
+        // *************************************************************
+        // Application:     The Conversation Starter Solution
+        // Author:          Velis, John E
+        // Date Created:    5/20/2016
+        // Date Revised:    7/7/2016
+        // *************************************************************
+
+        static void Main(string[] args)
+        {
+            //
+            // display a welcome to the user
+            //
+            Console.WriteLine("Greetings! Welcome to our conversation.");
+            Console.WriteLine();
+       
+            //
+            // begin your code
+            //
+
+
+            //
+            // display a prompt to exit the application
+            //
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit the application.");
+            Console.ReadKey();
+        }
+    }
+}
